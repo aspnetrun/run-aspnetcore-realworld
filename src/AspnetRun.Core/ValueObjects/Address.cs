a3dbf897da4a10921d1ee6c6adacedfee731ca93
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-27 16:04:41 +0300

    Address fixed

--- src/AspnetRun.Core/ValueObjects/Address.cs
+++ src/AspnetRun.Core/ValueObjects/Address.cs
@@ -8,8 +8,21 @@ namespace AspnetRun.Core.ValueObjects
     {
         public string AddressDesc { get; set; }
         public string City { get; set; }
-        public string Region { get; set; }
+        public string Region { get; set; }        
+        public string Country { get; set; }
         public string PostalCode { get; set; }
-        public string Country { get; set; }      
+
+        public Address()
+        {
+        }
+
+        public Address(string addressDesc, string city, string region, string country, string postalCode)
+        {
+            AddressDesc = addressDesc;
+            City = city;
+            Region = region;
+            Country = country;
+            PostalCode = postalCode;
+        }
     }
 }
