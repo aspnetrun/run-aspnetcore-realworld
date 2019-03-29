39bf32e5122102e93c0ef5d4a75484f45d62033b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-03-29 17:09:51 +0300

    ShoppingCartBuilder

---
+++ test/AspnetRun.Core.Tests/Builder/ShoppingCartBuilder.cs
@@ -0,0 +1,12 @@
+﻿using AspnetRun.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Core.Tests.Builder
+{
+    public class ShoppingCartBuilder
+    {
+        private ShoppingCart _shoppingCart;
+    }
+}
