c63c390241e48bbc264ffe69fae578371f241596
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-18 15:28:29 +0300

    Mock sc

---
+++ test/AspnetRun.Infrastructure.Tests/Repositories/ShoppingCartTest.cs
@@ -0,0 +1,23 @@
+﻿using AspnetRun.Core.Entities;
+using AspnetRun.Core.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Infrastructure.Tests.Repositories
+{
+    public class ShoppingCartTest
+    {
+        private Mock<IAsyncRepository<ShoppingCart>> _mockShoppingCartRepository;
+        private Mock<IAsyncRepository<ShoppingCartItem>> _mockShoppingCartItemRepository;
+
+        public ShoppingCartTest()
+        {
+            _mockShoppingCartRepository = new Mock<IAsyncRepository<ShoppingCart>>();
+            _mockShoppingCartItemRepository = new Mock<IAsyncRepository<ShoppingCartItem>>();
+        }
+
+        
+    }
+}
