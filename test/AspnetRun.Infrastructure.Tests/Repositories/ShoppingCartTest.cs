6eb2bec5cfcee1cc549340291dd08e86ae1d498d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-20 19:43:36 +0300

    Should_Get_Data

--- test/AspnetRun.Infrastructure.Tests/Repositories/ShoppingCartTest.cs
+++ test/AspnetRun.Infrastructure.Tests/Repositories/ShoppingCartTest.cs
@@ -1,9 +1,9 @@
 ﻿using AspnetRun.Core.Entities;
 using AspnetRun.Core.Interfaces;
 using Moq;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace AspnetRun.Infrastructure.Tests.Repositories
 {
@@ -18,6 +18,39 @@ namespace AspnetRun.Infrastructure.Tests.Repositories
             _mockShoppingCartItemRepository = new Mock<IAsyncRepository<ShoppingCartItem>>();
         }
 
-        
+        [Fact]
+        public async Task Should_Get_Data()
+        {
+            var shoppingCartList = new List<ShoppingCart>();
+            _mockShoppingCartRepository.Setup(x => x.GetAllAsync())
+                .ReturnsAsync(shoppingCartList);
+
+            Assert.True(shoppingCartList.Count > 0);
+        }
+
+        [Fact]
+        public async Task Should_InvokeShoppingCartRepo_Given_TwoItemsInCart()
+        {
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.AddItemToCart(1, It.IsAny<decimal>(), It.IsAny<short>());
+            shoppingCart.AddItemToCart(2, It.IsAny<decimal>(), It.IsAny<short>());
+
+            _mockShoppingCartRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(shoppingCart);
+
+
+            Assert.NotNull(shoppingCart);
+
+            _mockShoppingCartRepository.Verify(x => x.DeleteAsync(It.IsAny<ShoppingCart>()), Times.Once);
+            _mockShoppingCartItemRepository.Verify(x => x.DeleteAsync(It.IsAny<ShoppingCartItem>()), Times.Exactly(2));
+
+            
+            //Assert.NotNull(result);
+            //Assert.Equal(_testBasketId, result.Id);
+            //Assert.False(GetTestBasketCollection().AsQueryable().Any(spec.Criteria));
+            //Assert.Equal(expectedCount, result.Count());
+
+            await _mockShoppingCartItemRepository.Object.GetAllAsync();
+        }
     }
 }
