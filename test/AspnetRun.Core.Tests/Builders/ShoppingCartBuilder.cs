32d3d31bb25922be37c3a90de691897eefb33b36
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-20 22:23:22 +0300

    renamed

--- test/AspnetRun.Core.Tests/Builders/ShoppingCartBuilder.cs
+++ test/AspnetRun.Core.Tests/Builders/ShoppingCartBuilder.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace AspnetRun.Core.Tests.Builder
+namespace AspnetRun.Core.Tests.Builders
 {
     public class ShoppingCartBuilder
     {
