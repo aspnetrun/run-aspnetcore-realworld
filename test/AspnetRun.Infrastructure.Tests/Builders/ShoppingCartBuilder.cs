80af0fdb31368fcc82bff0174b0809ae3ee16c8e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-20 22:57:32 +0300

    ShoppingCartBuilder

---
+++ test/AspnetRun.Infrastructure.Tests/Builders/ShoppingCartBuilder.cs
@@ -0,0 +1,26 @@
+﻿using AspnetRun.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Infrastructure.Tests.Builders
+{
+    public class ShoppingCartBuilder
+    {
+        private ShoppingCart _shoppingCart;
+        public string TestBuyerId => "12345";
+        public int TestUserId => 234;
+        public string TestProductName => "Test Product Name";
+        public string TestPictureUri => "http://test.com/image.jpg";
+        public decimal TestUnitPrice = 1.23m;
+        public int TestUnits = 3;
+
+        public ShoppingCartBuilder()
+        {
+            _shoppingCart = new ShoppingCart
+            {
+                UserId = TestUserId
+            };
+        }
+    }
+}
--- test/AspnetRun.Infrastructure.Tests/Repositories/ShoppingCartTest.cs
+++ test/AspnetRun.Infrastructure.Tests/Repositories/ShoppingCartTest.cs
@@ -30,48 +30,10 @@ namespace AspnetRun.Infrastructure.Tests.Repositories
             _repository = new AspnetRunRepository<ShoppingCart>(_context);
         }
 
-        private Mock<IAsyncRepository<ShoppingCart>> _mockShoppingCartRepository;
-        private Mock<IAsyncRepository<ShoppingCartItem>> _mockShoppingCartItemRepository;
-
-        public ShoppingCartTest()
-        {
-            _mockShoppingCartRepository = new Mock<IAsyncRepository<ShoppingCart>>();
-            _mockShoppingCartItemRepository = new Mock<IAsyncRepository<ShoppingCartItem>>();
-        }
-
         [Fact]
-        public async Task Should_Get_Data()
+        public async Task Get_Existing_Shopping_Cart()
         {
-            var shoppingCartList = new List<ShoppingCart>();
-            _mockShoppingCartRepository.Setup(x => x.GetAllAsync())
-                .ReturnsAsync(shoppingCartList);
 
-            Assert.True(shoppingCartList.Count > 0);
-        }
-
-        [Fact]
-        public async Task Should_InvokeShoppingCartRepo_Given_TwoItemsInCart()
-        {
-            var shoppingCart = new ShoppingCart();
-            shoppingCart.AddItemToCart(1, It.IsAny<decimal>(), It.IsAny<short>());
-            shoppingCart.AddItemToCart(2, It.IsAny<decimal>(), It.IsAny<short>());
-
-            _mockShoppingCartRepository.Setup(x => x.GetByIdAsync(It.IsAny<int>()))
-                .ReturnsAsync(shoppingCart);
-
-
-            Assert.NotNull(shoppingCart);
-
-            _mockShoppingCartRepository.Verify(x => x.DeleteAsync(It.IsAny<ShoppingCart>()), Times.Once);
-            _mockShoppingCartItemRepository.Verify(x => x.DeleteAsync(It.IsAny<ShoppingCartItem>()), Times.Exactly(2));
-
-            
-            //Assert.NotNull(result);
-            //Assert.Equal(_testBasketId, result.Id);
-            //Assert.False(GetTestBasketCollection().AsQueryable().Any(spec.Criteria));
-            //Assert.Equal(expectedCount, result.Count());
-
-            await _mockShoppingCartItemRepository.Object.GetAllAsync();
         }
     }
 }
