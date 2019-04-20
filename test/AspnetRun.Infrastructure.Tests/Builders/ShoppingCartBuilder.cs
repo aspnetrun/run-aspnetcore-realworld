c5143988a092a9b9f690a7f55206c6bdb1ef8432
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-20 23:16:04 +0300

    ShoppingCartBuilder

--- test/AspnetRun.Infrastructure.Tests/Builders/ShoppingCartBuilder.cs
+++ test/AspnetRun.Infrastructure.Tests/Builders/ShoppingCartBuilder.cs
@@ -7,20 +7,22 @@ namespace AspnetRun.Infrastructure.Tests.Builders
 {
     public class ShoppingCartBuilder
     {
-        private ShoppingCart _shoppingCart;
-        public string TestBuyerId => "12345";
-        public int TestUserId => 234;
-        public string TestProductName => "Test Product Name";
-        public string TestPictureUri => "http://test.com/image.jpg";
-        public decimal TestUnitPrice = 1.23m;
-        public int TestUnits = 3;
+        private ShoppingCart _shoppingCart;        
+        public int TestUserId => 234;        
 
         public ShoppingCartBuilder()
         {
-            _shoppingCart = new ShoppingCart
+            _shoppingCart = WithDefaultValues();
+        }
+
+        public ShoppingCart WithDefaultValues()
+        {
+            var shoppingCart = new ShoppingCart
             {
                 UserId = TestUserId
             };
+
+            return shoppingCart;
         }
     }
 }
