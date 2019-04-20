355e30306620a1e77763b6e14850b534bceb10d5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-20 22:43:14 +0300

    ITestOutputHelper

--- test/AspnetRun.Infrastructure.Tests/Repositories/ShoppingCartTest.cs
+++ test/AspnetRun.Infrastructure.Tests/Repositories/ShoppingCartTest.cs
@@ -1,17 +1,34 @@
 ﻿using AspnetRun.Core.Entities;
 using AspnetRun.Core.Interfaces;
 using AspnetRun.Infrastructure.Data;
+using AspnetRun.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace AspnetRun.Infrastructure.Tests.Repositories
 {
     public class ShoppingCartTest
     {
         private readonly AspnetRunContext _context;
+        private readonly IAsyncRepository<ShoppingCart> _repository;
+        private readonly ITestOutputHelper _testOutput;
 
+        public ShoppingCartTest(ITestOutputHelper testOutput)
+        {
+            _testOutput = testOutput ?? throw new ArgumentNullException(nameof(testOutput));
+
+            var options = new DbContextOptionsBuilder<AspnetRunContext>()
+                .UseInMemoryDatabase(databaseName: "AspnetRunRealWorld")
+                .Options;
+
+            _context = new AspnetRunContext(options);
+            _repository = new AspnetRunRepository<ShoppingCart>(_context);
+        }
 
         private Mock<IAsyncRepository<ShoppingCart>> _mockShoppingCartRepository;
         private Mock<IAsyncRepository<ShoppingCartItem>> _mockShoppingCartItemRepository;
