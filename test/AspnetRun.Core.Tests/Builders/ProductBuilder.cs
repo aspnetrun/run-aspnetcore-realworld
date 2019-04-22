c7626527ed1728ee7c74f29a8b1b06bb3866bb02
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-22 18:15:08 +0300

    ProductTests

---
+++ test/AspnetRun.Core.Tests/Builders/ProductBuilder.cs
@@ -0,0 +1,27 @@
+﻿using AspnetRun.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Core.Tests.Builders
+{
+    public class ProductBuilder
+    {
+        public int ProductId1 => 123;
+        public int ProductId2 => 124;
+        public int ProductId3 => 125;
+        public string ProductName1 => "ProductX";
+        public string ProductName2 => "ProductY";
+        public string ProductName3 => "ProductZ";
+
+        public List<Product> GetProductCollection()
+        {
+            return new List<Product>()
+            {
+                //Product.Create(ProductId1, 1, ProductName1),
+                //Product.Create(ProductId2, 1, ProductName2),
+                //Product.Create(ProductId3, 1, ProductName3)
+            };
+        }
+    }
+}
