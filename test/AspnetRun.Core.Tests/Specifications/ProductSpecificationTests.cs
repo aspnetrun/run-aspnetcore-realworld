3c9a97e8dfbf7556f8bfbe816ac303df1ac05564
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-04-22 18:17:22 +0300

    ProductSpecificationTests

---
+++ test/AspnetRun.Core.Tests/Specifications/ProductSpecificationTests.cs
@@ -0,0 +1,25 @@
+﻿using AspnetRun.Core.Specifications;
+using AspnetRun.Core.Tests.Builders;
+using System.Linq;
+using Xunit;
+
+namespace AspnetRun.Core.Tests.Specifications
+{
+    public class ProductSpecificationTests
+    {
+        private ProductBuilder ProductBuilder { get; } = new ProductBuilder();
+
+        [Fact]
+        public void Matches_Product_With_Category_Spec()
+        {
+            var spec = new ProductWithCategorySpecification(ProductBuilder.ProductName1);
+
+            var result = ProductBuilder.GetProductCollection()
+                .AsQueryable()
+                .FirstOrDefault(spec.Criteria);
+
+            Assert.NotNull(result);
+            Assert.Equal(ProductBuilder.ProductId1, result.Id);
+        }
+    }
+}
