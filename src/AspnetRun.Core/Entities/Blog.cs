5f7bea262c7851de6c052adf60336b29f497fbd6
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-25 15:31:03 +0300

    Blog added

---
+++ src/AspnetRun.Core/Entities/Blog.cs
@@ -0,0 +1,17 @@
+﻿using AspnetRun.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AspnetRun.Core.Entities
+{
+    public class Blog : Entity
+    {
+        [Required, StringLength(80)]
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+    }
+}
