a8b0cd1176ea3ebacb9e1d03162fea9bcbf7ec1f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-05-25 16:16:06 +0300

    Order entity created

---
+++ src/AspnetRun.Core/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using AspnetRun.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspnetRun.Core.Entities
+{
+    public class Order : Entity
+    {
+        // Checkout
+        // Track Order
+
+        // TODO : Order Capture and Order Fullfilment related tasks should be handled in here.
+    }
+}
