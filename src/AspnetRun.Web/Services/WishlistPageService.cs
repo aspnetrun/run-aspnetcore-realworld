66e27a20cbd43eb7e99a906c3336f7b5ae99eb50
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-08 15:23:18 +0300

    WishlistPageService edited

--- src/AspnetRun.Web/Services/WishlistPageService.cs
+++ src/AspnetRun.Web/Services/WishlistPageService.cs
@@ -1,15 +1,30 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using AspnetRun.Web.Interfaces;
 using AspnetRun.Web.ViewModels;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
 
 namespace AspnetRun.Web.Services
 {
     public class WishlistPageService : IWishlistPageService
     {
+        private readonly IWishListService _wishListAppService;
+        private readonly IMapper _mapper;
+        private readonly ILogger<WishlistPageService> _logger;
 
-        public Task<WishlistViewModel> GetWishlist(string userName)
+        public WishlistPageService(IWishListService wishListAppService, IMapper mapper, ILogger<WishlistPageService> logger)
         {
-            throw new System.NotImplementedException();
+            _wishListAppService = wishListAppService ?? throw new ArgumentNullException(nameof(wishListAppService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<WishlistViewModel> GetWishlist(string userName)
+        {
+            var product = await _wishListAppService.GetProductByUserName(userName);
+            var mapped = _mapper.Map<WishlistViewModel>(product);
+            return mapped;
         }
     }
 }
