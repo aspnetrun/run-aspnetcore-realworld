993100edbe5fd22c2a2efaee500d69d1db3790c9
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-10 13:15:49 +0300

    IComparePageService added

---
+++ src/AspnetRun.Web/Interfaces/IComparePageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRun.Web.Interfaces
+{
+    public interface IComparePageService
+    {
+        Task<CompareViewModel> GetCompare(string userName);
+        Task RemoveItem(int compareId, int productId);
+        Task AddToCart(string userName, int productId);
+    }
+}
--- src/AspnetRun.Web/Pages/Compare.cshtml.cs
+++ src/AspnetRun.Web/Pages/Compare.cshtml.cs
@@ -2,15 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspnetRun.Web.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspnetRun.Web.Pages
 {
+    [Authorize]
     public class CompareModel : PageModel
     {
-        public void OnGet()
+        private readonly IComparePageService _compareService;
+
+        public CompareModel(IComparePageService CompareService)
         {
+            _compareService = CompareService ?? throw new ArgumentNullException(nameof(CompareService));
+        }
+
+        public CompareViewModel CompareViewModel { get; set; } = new CompareViewModel();
+
+        public async Task OnGet()
+        {
+            var userName = this.User.Identity.Name;
+            CompareViewModel = await _compareService.GetCompare(userName);
+        }
+
+        public async Task<IActionResult> OnPostRemoveFromCompare(int compareId, int productId)
+        {
+            await _compareService.RemoveItem(compareId, productId);
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostAddToCart(string userName, int productId)
+        {
+            await _compareService.AddToCart(userName, productId);
+            return RedirectToPage();
         }
     }
 }
