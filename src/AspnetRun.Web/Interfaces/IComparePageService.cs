199229fa42ccd86f075149e1b8dd0a1442ead57f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-10 13:17:21 +0300

    refactor compare

--- src/AspnetRun.Web/Interfaces/IComparePageService.cs
+++ src/AspnetRun.Web/Interfaces/IComparePageService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using AspnetRun.Web.ViewModels;
 using System.Threading.Tasks;
 
 namespace AspnetRun.Web.Interfaces
--- src/AspnetRun.Web/Pages/Compare.cshtml.cs
+++ src/AspnetRun.Web/Pages/Compare.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AspnetRun.Web.Interfaces;
+using AspnetRun.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
