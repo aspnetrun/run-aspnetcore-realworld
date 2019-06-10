07c963ad77c553ad047c6994deae1cba68e95ba5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-10 13:21:24 +0300

    ComparePageService added

---
+++ src/AspnetRun.Web/Services/ComparePageService.cs
@@ -0,0 +1,40 @@
+﻿using AspnetRun.Web.Interfaces;
+using AspnetRun.Web.ViewModels;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRun.Web.Services
+{
+    public class ComparePageService : IComparePageService
+    {
+        private readonly ICompareService _compareAppService;
+        private readonly IMapper _mapper;
+        private readonly ILogger<ComparePageService> _logger;
+
+        public ComparePageService(ICompareService compareAppService, IMapper mapper, ILogger<ComparePageService> logger)
+        {
+            _compareAppService = compareAppService ?? throw new ArgumentNullException(nameof(compareAppService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }       
+
+        public Task<CompareViewModel> GetCompare(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveItem(int compareId, int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddToCart(string userName, int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
