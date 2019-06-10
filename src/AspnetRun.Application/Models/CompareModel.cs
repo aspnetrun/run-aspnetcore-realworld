24f49ecef908370f5a2e36dc85a9cbe25358310f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-10 13:30:28 +0300

    CompareModel added

---
+++ src/AspnetRun.Application/Models/CompareModel.cs
@@ -0,0 +1,11 @@
+﻿using AspnetRun.Application.Models.Base;
+using System.Collections.Generic;
+
+namespace AspnetRun.Application.Models
+{
+    public class CompareModel : BaseModel
+    {
+        public string UserName { get; set; }
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+    }
+}
