c527373f5c9677f6665a8010e3ecd8007962ed5c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-10 13:16:43 +0300

    CompareViewModel added

---
+++ src/AspnetRun.Web/ViewModels/CompareViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AspnetRun.Web.ViewModels.Base;
+using System.Collections.Generic;
+
+namespace AspnetRun.Web.ViewModels
+{
+    public class CompareViewModel : BaseViewModel
+    {
+        public string UserName { get; set; }
+        public List<ProductViewModel> Items { get; set; } = new List<ProductViewModel>();
+    }
+}
