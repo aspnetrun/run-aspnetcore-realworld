f71dff578f9131054ac287acf2f7851776824a5c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-13 19:17:27 +0300

    CartWithItemsSpecification added

---
+++ src/AspnetRun.Core/Specifications/CartWithItemsSpecification.cs
@@ -0,0 +1,20 @@
+﻿using AspnetRun.Core.Entities;
+using AspnetRun.Core.Specifications.Base;
+
+namespace AspnetRun.Core.Specifications
+{
+    public class CartWithItemsSpecification : BaseSpecification<Cart>
+    {
+        public CartWithItemsSpecification(string userName)
+            : base(p => p.UserName.ToLower().Contains(userName.ToLower()))
+        {
+            AddInclude(p => p.Items);
+        }
+
+        public CartWithItemsSpecification(int cartId)
+            : base(p => p.Id == cartId)
+        {
+            AddInclude(p => p.Items);
+        }
+    }   
+}
