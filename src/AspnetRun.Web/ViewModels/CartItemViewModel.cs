2fae770048cd0b427f0bfb6a327291827ce25083
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-21 17:11:23 +0300

    CartItemViewModel added

---
+++ src/AspnetRun.Web/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AspnetRun.Web.ViewModels.Base;
+
+namespace AspnetRun.Web.ViewModels
+{
+    public class CartItemViewModel : BaseViewModel
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int ProductId { get; set; }
+        public ProductViewModel Product { get; set; }
+    }
+}
