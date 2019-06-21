3aee8896e51d7cacf8da80afb27beb3d02c3d44d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-21 17:21:26 +0300

    ICartPageService created

---
+++ src/AspnetRun.Web/Interfaces/ICartPageService.cs
@@ -0,0 +1,12 @@
+﻿using AspnetRun.Web.ViewModels;
+using System.Threading.Tasks;
+
+namespace AspnetRun.Web.Interfaces
+{
+    public interface ICartPageService
+    {
+        Task<CartViewModel> GetCart(string userName);
+        Task AddItem(string userName, int productId);
+        Task RemoveItem(int cartId, int productId);
+    }
+}
--- src/AspnetRun.Web/Pages/Components/ShoppingCart/ShoppingCart.cs
+++ src/AspnetRun.Web/Pages/Components/ShoppingCart/ShoppingCart.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using AspnetRun.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AspnetRun.Web.Pages.Components.ShoppingCart
 {
-    public class ShoppingCart
+    public class ShoppingCart : ViewComponent
     {
+        private readonly ICartPageService  _cartPageService;
+
+        public ShoppingCart(ICartPageService cartPageService)
+        {
+            _cartPageService = cartPageService ?? throw new ArgumentNullException(nameof(cartPageService));
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var vm = new CartViewModel();
+            //vm = await GetCompareViewModelAsync();
+            return View(vm);
+        }
+
+
+
+        //public WishlistViewModel WishlistViewModel { get; set; } = new WishlistViewModel();
+
+        //public async Task OnGetAsync()
+        //{
+        //    var userName = this.User.Identity.Name;
+        //    WishlistViewModel = await _wishlistPageService.GetWishlist(userName);
+        //}
+
+        //public async Task<IActionResult> OnPostAddToCartAsync(string userName, int productId)
+        //{
+        //    await _wishlistPageService.AddToCart(userName, productId);
+        //    return RedirectToPage();
+        //}
+
+        //public async Task<IActionResult> OnPostRemoveFromWishlistAsync(int wishlistId, int productId)
+        //{
+        //    await _wishlistPageService.RemoveItem(wishlistId, productId);
+        //    return RedirectToPage();
+        //}
     }
 }
