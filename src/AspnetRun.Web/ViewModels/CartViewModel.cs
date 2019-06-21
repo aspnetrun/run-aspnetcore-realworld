273439ad26498c0bdaec9b948931e248dda02008
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-21 17:10:03 +0300

    CartViewModel added

---
+++ src/AspnetRun.Web/ViewModels/CartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRun.Web.ViewModels
+{
+    public class CartViewModel : BaseViewModel
+    {
+        public string UserName { get; set; }
+        public List<CartItemViewModel> Items { get; set; } = new List<CartItemViewModel>();
+    }
+}
