460685faa018531e5d267a726f051081c2c10021
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-23 18:48:46 +0300

    OrderItemView added

---
+++ src/AspnetRun.Web/ViewModels/OrderItemView.cs
@@ -0,0 +1,12 @@
+﻿namespace AspnetRun.Web.ViewModels
+{
+    public class OrderItemView
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int ProductId { get; set; }
+        public ProductViewModel Product { get; set; }
+    }
+}
--- src/AspnetRun.Web/ViewModels/OrderViewModel.cs
+++ src/AspnetRun.Web/ViewModels/OrderViewModel.cs
@@ -8,6 +8,43 @@ namespace AspnetRun.Web.ViewModels
     public class OrderViewModel
     {
         public string UserName { get; set; }
-        public List<CartItemViewModel> Items { get; set; } = new List<CartItemViewModel>();
+        public AddressView BillingAddress { get; set; }
+        public AddressView ShippingAddress { get; set; }
+        public PaymentMethodView PaymentMethod { get; set; }
+        public OrderStatusView Status { get; set; }
+        public decimal GrandTotal { get; set; }
+
+        public List<OrderItemView> Items { get; set; } = new List<OrderItemView>();
     }
+
+    public class AddressView
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNo { get; set; }
+        public string CompanyName { get; set; }
+        public string AddressLine { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+
+    public enum OrderStatusView
+    {
+        Progress = 1,
+        OnShipping = 2,
+        Finished = 3
+    }
+
+    public enum PaymentMethodView
+    {
+        Check = 1,
+        BankTransfer = 2,
+        Cash = 3,
+        Paypal = 4,
+        Payoneer = 5
+    }
+
 }
