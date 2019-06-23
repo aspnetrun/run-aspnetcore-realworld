7f0caa527bdef79e4a8afc8dc138d77bbda429b3
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-23 14:34:37 +0300

    GrandTotal added

--- src/AspnetRun.Web/ViewModels/CartViewModel.cs
+++ src/AspnetRun.Web/ViewModels/CartViewModel.cs
@@ -7,5 +7,19 @@ namespace AspnetRun.Web.ViewModels
     {
         public string UserName { get; set; }
         public List<CartItemViewModel> Items { get; set; } = new List<CartItemViewModel>();
+
+        public decimal GrandTotal
+        {
+            get
+            {
+                decimal grandTotal = 0;
+                foreach (var item in Items)
+                {
+                    grandTotal += item.TotalPrice;
+                }
+
+                return grandTotal;
+            }
+        }
     }
 }
