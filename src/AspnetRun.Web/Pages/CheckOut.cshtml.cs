93a2a947bbd48d12c2ad77c5f6bddd6fea80c01b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-23 17:28:48 +0300

    ICheckOutPageService and checkout page

--- src/AspnetRun.Web/Pages/CheckOut.cshtml.cs
+++ src/AspnetRun.Web/Pages/CheckOut.cshtml.cs
@@ -9,8 +9,27 @@ namespace AspnetRun.Web.Pages
 {
     public class CheckOutModel : PageModel
     {
-        public void OnGet()
+        private readonly ICheckOutPageService _checkOutPageService;
+
+        public CheckOutModel(ICheckOutPageService checkOutPageService)
         {
+            _checkOutPageService = checkOutPageService ?? throw new ArgumentNullException(nameof(checkOutPageService));
+        }
+
+        public IEnumerable<ProductViewModel> ProductList { get; set; } = new List<ProductViewModel>();
+
+        public async Task OnGetAsync()
+        {
+            ProductList = await _productPageService.GetProducts(SearchTerm);
+        }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToPage("./Account/Login", new { area = "Identity" });
+
+            await _productPageService.AddToCart(User.Identity.Name, productId);
+            return RedirectToPage();
         }
     }
 }
