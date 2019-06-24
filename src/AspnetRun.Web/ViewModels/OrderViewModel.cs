b7cea395b798aed89433a4f72c815d32db807435
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-24 14:44:11 +0300

    AddressViewRequired

--- src/AspnetRun.Web/ViewModels/OrderViewModel.cs
+++ src/AspnetRun.Web/ViewModels/OrderViewModel.cs
@@ -7,7 +7,7 @@ namespace AspnetRun.Web.ViewModels
     {
         public string UserName { get; set; }
         [Required]
-        public AddressView BillingAddress { get; set; }
+        public AddressViewRequired BillingAddress { get; set; }
         public AddressView ShippingAddress { get; set; }
         public PaymentMethodView PaymentMethod { get; set; }
         public decimal GrandTotal { get; set; }
@@ -15,10 +15,33 @@ namespace AspnetRun.Web.ViewModels
         public List<OrderItemView> Items { get; set; } = new List<OrderItemView>();
     }
 
-    public class AddressView
+    public class AddressViewRequired
     {
         [Required]
-        public string FirstName { get; set; }        
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string PhoneNo { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string AddressLine { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+    }
+
+    public class AddressView
+    {
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public string PhoneNo { get; set; }
