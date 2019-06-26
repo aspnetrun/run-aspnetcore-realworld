37ccb8d29a27d6aeb6a14f33d4056bb5c2560542
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-26 15:12:07 +0300

    IOrderService

--- src/AspnetRun.Application/Interfaces/IOrderService.cs
+++ src/AspnetRun.Application/Interfaces/IOrderService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AspnetRun.Application.Interfaces
 {
     public interface IOrderService
     {
         // TODO : implement this
+        Task CheckOut(string userName, int productId);
     }
 }
