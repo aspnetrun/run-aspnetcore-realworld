409808ac6273a6f98d7b578b6a7d613453ba481f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-26 15:13:21 +0300

    OrderService implementation

--- src/AspnetRun.Application/Interfaces/IOrderService.cs
+++ src/AspnetRun.Application/Interfaces/IOrderService.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace AspnetRun.Application.Interfaces
 {
     public interface IOrderService
-    {
-        // TODO : implement this
-        Task CheckOut(string userName, int productId);
+    {        
+        Task CheckOut(string userName, int orderId);
     }
 }
---
+++ src/AspnetRun.Application/Services/OrderService.cs
@@ -0,0 +1,14 @@
+﻿using AspnetRun.Application.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace AspnetRun.Application.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task CheckOut(string userName, int orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
