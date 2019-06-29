92b93282a011df6ffc6afe6f3838c6a2d95df889
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-29 13:18:47 +0300

    OrderRepository added

--- src/AspnetRun.Core/Repositories/IOrderRepository.cs
+++ src/AspnetRun.Core/Repositories/IOrderRepository.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using AspnetRun.Core.Entities;
+using AspnetRun.Core.Repositories.Base;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace AspnetRun.Core.Repositories
 {
-    public interface IOrderRepository
+    public interface IOrderRepository : IRepository<Order>
     {
+        Task<IEnumerable<Order>> GetOrderByUserNameAsync(string userName);
     }
 }
---
+++ src/AspnetRun.Infrastructure/Repository/OrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using AspnetRun.Core.Entities;
+using AspnetRun.Core.Repositories;
+using AspnetRun.Infrastructure.Data;
+using AspnetRun.Infrastructure.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRun.Infrastructure.Repository
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(AspnetRunContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Task<IEnumerable<Order>> GetOrderByUserNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
