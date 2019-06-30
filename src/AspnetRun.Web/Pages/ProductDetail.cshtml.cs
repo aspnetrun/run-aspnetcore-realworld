46e79fcbe3b960edd61bb40d1f9f9bb06a3534b0
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-06-30 15:33:26 +0300

    ProductDetailModel

--- src/AspnetRun.Web/Pages/ProductDetail.cshtml.cs
+++ src/AspnetRun.Web/Pages/ProductDetail.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspnetRun.Web.Interfaces;
+using AspnetRun.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,47 @@ namespace AspnetRun.Web.Pages
 {
     public class ProductDetailModel : PageModel
     {
-        public void OnGet()
+        private readonly IProductPageService _productPageService;
+
+        public ProductDetailModel(IProductPageService productService)
         {
+            _productPageService = productService ?? throw new ArgumentNullException(nameof(productService));
         }
+
+        public ProductViewModel Product { get; set; } = new ProductViewModel();        
+
+        public async Task OnGetAsync()
+        {
+            //Product = await _productPageService.GetProducts(SearchTerm);
+        }
+
+        //public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        //{
+        //    if (!User.Identity.IsAuthenticated)
+        //        return RedirectToPage("./Account/Login", new { area = "Identity" });
+
+        //    await _productPageService.AddToCart(User.Identity.Name, productId);
+        //    return RedirectToPage();
+        //}
+
+        //public async Task<IActionResult> OnPostAddToWishlistAsync(int productId)
+        //{
+        //    if (!User.Identity.IsAuthenticated)
+        //        return RedirectToPage("./Account/Login", new { area = "Identity" });
+
+        //    await _productPageService.AddToWishlist(User.Identity.Name, productId);
+        //    return RedirectToPage();
+        //}
+
+        //public async Task<IActionResult> OnPostAddToCompareAsync(int productId)
+        //{
+        //    if (!User.Identity.IsAuthenticated)
+        //        return RedirectToPage("./Account/Login", new { area = "Identity" });
+
+        //    await _productPageService.AddToCompare(User.Identity.Name, productId);
+        //    return RedirectToPage();
+        //}
+
+
     }
 }
