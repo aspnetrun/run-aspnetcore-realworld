095e1b8f6b2c6c2f8d627fc673b61e58ac93b8af
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-07-02 08:09:43 +0300

    Index page cart component added

--- src/AspnetRun.Web/Pages/Index.cshtml.cs
+++ src/AspnetRun.Web/Pages/Index.cshtml.cs
@@ -11,10 +11,11 @@ namespace AspnetRun.Web.Pages
     public class IndexModel : PageModel
     {
         private readonly IIndexPageService _indexPageService;
+        private readonly ICartComponentService _cartComponentService;
 
-        public IndexModel(IIndexPageService indexPageService)
+        public IndexModel(IIndexPageService indexPageService, ICartComponentService cartComponentService) : this(indexPageService)
         {
-            _indexPageService = indexPageService ?? throw new ArgumentNullException(nameof(indexPageService));
+            _cartComponentService = cartComponentService ?? throw new ArgumentNullException(nameof(cartComponentService));
         }
 
         public IEnumerable<ProductViewModel> ProductList { get; set; } = new List<ProductViewModel>();
@@ -23,6 +24,12 @@ namespace AspnetRun.Web.Pages
         public async Task OnGetAsync()
         {
             ProductList = await _indexPageService.GetProducts();              
+        }
+
+        public async Task<IActionResult> OnPostRemoveToCartAsync(int cartId, int cartItemId)
+        {
+            await _cartComponentService.RemoveItem(cartId, cartItemId);
+            return RedirectToPage();
         }
     }
 }
